97d1f0866af85f4c52b6610c1af7042e42c00bdc
bayashi <naoya_nstyle@hotmail.co.jp>
2022-04-28 18:21:32 +0900

    del a line

--- tweet-widget.js
+++ tweet-widget.js
@@ -30,7 +30,6 @@ reearth.ui.show(
       link.setAttribute('data-hashtags', property.hashtags);
       link.setAttribute('data-text', property.text);
     }
-    e.source.reearth.on();
   });
 
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
